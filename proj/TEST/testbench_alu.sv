9e7d22bed8e0a52a08585131a6ea1d5b1a07e9b0
Kacper <01178380@pw.edu.pl>
2024-01-23 21:19:22 +0100

    Zmodyfikowano dokumantację, dodano testbench do alu ale do rozpisania na więcej przypadków, trzeba jeszcze raz spojrzeć na wejścia i_op

--- proj/TEST/testbench_alu.sv
+++ proj/TEST/testbench_alu.sv
@@ -1,8 +1,6 @@
 `timescale 1ps/1ps
 `include "./../CODE/MODULES/new_alu.sv"
 
-/*Moduł służący do testowania funkcjonalności 'alu.sv'*/
-
 module testbench;
 
     parameter s_N = 4;
@@ -18,23 +16,33 @@ module testbench;
     wire [s_K-1:0] simulation_s_result;
     wire [3:0] simulation_s_status;
 
-    wire [s_K-1:0] synthesis_s_result;
-    wire [3:0] synthesis_s_status;
-    
     new_alu #(.N(s_N), .M(s_M), .K(s_K))
     new_alu_model_simulation (.i_arg_A(s_arg_A), .i_arg_B(s_arg_B), .i_op(s_op), .i_clk(s_clk), .i_reset(s_reset), .o_result(simulation_s_result), .o_status(simulation_s_status));
-    
 
     initial begin
         $dumpfile("signals.vcd");
         $dumpvars(0, testbench);
                 
-        s_op = 2'b10;
+        s_op = 4'b1000;
         s_reset = 1'b1;
-        
-        /*1.) Testowanie operacji Y = (A >> ~B)*/
+        s_clk = 0;
+        s_arg_A = 8'b11001100;
+        s_arg_B = 8'b11111110;
+        #1
+        s_clk = 1;
+        #1
 
-        /*Przesunięcie o 1 bit w prawo*/
+        s_op = 2'b00;
+        s_reset = 1'b1;
+        s_clk = 0;
+        s_arg_A = 8'b11001100;
+        s_arg_B = 8'b11111110;
+        #1
+        s_clk = 1;
+        #1
+
+        s_op = 4'b01;
+        s_reset = 1'b1;
         s_clk = 0;
         s_arg_A = 8'b11001100;
         s_arg_B = 8'b11111110;
@@ -43,6 +51,6 @@ module testbench;
         #1
 
         $finish
-	end
+    end
 
 endmodule
