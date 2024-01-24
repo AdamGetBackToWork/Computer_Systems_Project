e9c32cc79de9247e323d1a959696cbd67d1aaf9d
Maciej <gojski.maciej@gmail.com>
2024-01-24 20:41:11 +0100

    Zmodyfikowano ALU do jednego pliku i utworzono testbencha

--- proj/CODE/MODULES/new_alu.sv
+++ proj/CODE/MODULES/new_alu.sv
@@ -1,17 +1,585 @@
-`include "./../SUBMODULES/MACIEK/addition.sv"
-`include "./../SUBMODULES/MACIEK/division.sv"
-`include "./../SUBMODULES/MACIEK/right_shift.sv"
-`include "./../SUBMODULES/MACIEK/zm_to_u2.sv"
+module addition_1(i_arg_A, i_arg_B, cache_status, cache_result);
+	parameter M = 8;
+	parameter K = 8;
+	
+	input logic [M-1:0] i_arg_A;
+	input logic [M-1:0] i_arg_B;
+	output logic [K-1:0] cache_result;
+	output logic [3:0] cache_status;
+	
+	/*Wewnętrzny rejestr przechowujący ~B*/
+		
+	logic [M-1:0] cache_B;
+	
+	/*Wewnętrzny rejestr do przechowywania wyniku dodawania (większy o 1 bit, aby sprawdzić, czy nastąpiło przepełnienie)*/
+	
+	logic [M:0] cache;
+	
+	always_comb begin
+		
+		cache_status = 4'b0;
+		cache_result = 8'b0;
+		
+		/*Od razu zapisałem negację argumentu B, bo mi się myliło*/
+		
+		cache_B = ~(i_arg_B);
+        
+		/*Jeżeli obie liczby są dodatnie: wykonaj normalne dodawanie*/
+		
+		if((i_arg_A[M-1] == 0) & (cache_B[M-1] == 0)) begin
+			cache = i_arg_A + cache_B;
+			if(cache[M-1] == 1) begin
+				cache_status = 4'b1001;
+				cache_result = 8'bx;
+			end else begin
+				cache_result = cache[M-1:0];
+			end
+		end
+        	
+		/*Jeżeli tylko jedna liczba jest ujemna to odejmujemy je od siebie i sprawdzamy znak*/
+        
+		if((i_arg_A[M-1] == 0) & (cache_B[M-1] == 1)) begin
+			if((i_arg_A[M-2:0]) > cache_B[M-2:0]) begin
+				cache_result = {1'b0, (i_arg_A[M-2:0] - cache_B[M-2:0])};
+			end else begin
+				cache_result = {1'b1, (cache_B[M-2:0] - i_arg_A[M-2:0])};
+			end
+		end
+        	
+        /*To samo co powyżej*/
+        
+		if((i_arg_A[M-1] == 1) & (cache_B[M-1] == 0)) begin
+			if((i_arg_A[M-2:0]) > cache_B[M-2:0]) begin
+				cache_result = {1'b1, (i_arg_A[M-2:0] - cache_B[M-2:0])};
+			end else begin
+				cache_result = {1'b0, (cache_B[M-2:0] - i_arg_A[M-2:0])};
+			end
+		end
+        	
+		/*Jeżeli obie liczby są ujemne: dodajemy bez znaku i dostawiamy znak*/
+        	
+		if((i_arg_A[M-1] == 1) & (cache_B[M-1] == 1)) begin
+			cache = i_arg_A[M-2:0] + cache_B[M-2:0];
+			if(cache[M-1] == 1) begin
+				cache_status = 4'b1001;
+				cache_result = 8'bx;
+			end else begin
+				cache_result = {1'b1, cache[M-2:0]};
+			end
+		end
+	end
+endmodule
 
-`include "./../SUBMODULES/ADAM/sub_and_mul.sv"
-`include "./../SUBMODULES/ADAM/comparison_2.sv"
-`include "./../SUBMODULES/ADAM/add_bit.sv"
-`include "./../SUBMODULES/ADAM/u2_to_zm.sv"
+module division_1(i_arg_A, i_arg_B, cache_status, cache_result);
+	parameter M = 8;
+	parameter K = 8;
+	
+	input logic [M-1:0] i_arg_A;
+	input logic [M-1:0] i_arg_B;
+	output logic [K-1:0] cache_result;
+	output logic [3:0] cache_status;
+	
+	always_comb begin
+		
+		cache_status = 4'b0;
+		cache_result = 8'b0;
+		
+		/*Jeżeli dzielnik jest równy zero: zwracamy błąd*/
+            	
+		if(i_arg_B == 8'b0) begin
+			cache_status = 4'b1000;
+			cache_result = 8'bx;
+		end
 
-`include "./../SUBMODULES/KACPER/absolute.sv"
-`include "./../SUBMODULES/KACPER/comparison_1.sv"
-`include "./../SUBMODULES/KACPER/division.sv"
-`include "./../SUBMODULES/KACPER/right_shift_2.sv"
+		/*W przeciwnym przypadku zwracamy wynik*/
+		
+		else begin
+			cache_result = {(i_arg_A[M-1] ^ i_arg_B[M-1]), (i_arg_A[M-2:0] / i_arg_B[M-2:0])};
+		end
+	end	
+endmodule
+
+module right_shift_1(i_arg_A, i_arg_B, cache_status, cache_result);
+	parameter M = 8;
+	parameter K = 8;
+	
+	input logic [M-1:0] i_arg_A;
+	input logic [M-1:0] i_arg_B;
+	output logic [K-1:0] cache_result;
+	output logic [3:0] cache_status;
+	
+	always_comb begin
+		
+		cache_status = 4'b0;
+		cache_result = 8'b0;
+		
+		/*Sprawdzamy czy liczba nie stanie się ujemna po negacji
+		  Jeśli będzie, to wystawiamy odpowiedni kod błędu na 'o_status' oraz stan nieustalony na 'o_result'*/
+
+		if(i_arg_B[M-1] == 0) begin
+			cache_status = 4'b1000;
+			cache_result = 8'bx;
+		end
+		
+		/*Jeżeli wszystko jest git - przechodzimy do wykonania operacji*/
+		
+		else begin
+			cache_result = i_arg_A >> ~(i_arg_B);
+		end
+	end
+	
+endmodule
+
+module zm_to_u2(i_arg_A, i_arg_B, cache_status, cache_result);
+	parameter M = 8;
+	parameter K = 8;
+	
+	input logic [M-1:0] i_arg_A;
+	input logic [M-1:0] i_arg_B;
+	output logic [K-1:0] cache_result;
+	output logic [3:0] cache_status;
+	
+	always_comb begin
+		
+		cache_status = 4'b0;
+		cache_result = 8'b0;
+		
+		/*Jeżeli liczba nie jest ujemna: zwracamy tą liczbę bez zmian*/
+        	
+		if(i_arg_A[M-1] == 0) begin
+			cache_result = i_arg_A;
+		end
+
+		/*W przeciwnym przypadku zwracamy to co w algorytmie*/
+
+		else begin
+			cache_result = {1'b1, ~(i_arg_A[M-2:0]) + 1'b1};
+		end
+
+	end
+	
+endmodule
+
+module absolute (i_arg_B, o_newB, o_status);
+    parameter M = 8;
+    parameter K = 8;
+    input logic signed [M-1:0] i_arg_B;
+    output logic [K-1:0] o_newB;
+    output logic [3:0] o_status;
+    integer a;
+    integer x;
+
+    always_comb
+    begin
+        // Initialize outputs and variables
+        o_newB = '0;
+        x = 0;
+        a = 0;
+        o_status = '0;
+         
+        begin
+            if (i_arg_B >= 0) 
+                begin
+                    o_newB = i_arg_B;
+                end 
+            else 
+                begin
+                    o_newB = -i_arg_B;
+                end
+        end
+        
+        // Check if all bits in o_newB are ones
+        if (o_newB === (1 << M) - 1)
+            o_status[2] = 1; // Set status bit 2 to 1 if all bits in o_newB are ones
+
+        // Count the number of '1' bits in o_newB
+        for (a = 0; a < M; a++)
+        begin
+            if (o_newB[a] == 1)
+                x = x + 1;
+        end
+
+        // Check if the number of '1' bits is even and o_newB is not '0'
+        if (x % 2 == 0 && o_newB != 0)
+            o_status[1] = 1; // Set status bit 1 to 1 if conditions are met
+            
+        // Check if the number couldn't be converted properly
+        if (i_arg_B < -(2**(M-1)) || i_arg_B >= 2**(M-1)) begin
+            o_status[0] = 1; // Set status bit 0 to 1 if the number couldn't be converted properly
+        end
+    end
+endmodule
+
+module comparison_1 (i_arg_A, i_arg_B, o_eqA, o_status);
+    parameter M = 8;
+    parameter K = 8;
+    input logic signed [M-1:0] i_arg_A;
+    input logic signed [M-1:0] i_arg_B;
+    output logic [K-1:0] o_eqA;
+    output logic [3:0] o_status;
+    integer a;
+    integer x;
+
+    always_comb 
+    begin
+
+        o_status = '0;
+        o_eqA = '0;
+        a = 0;
+        x = 0;
+
+        if ((~i_arg_A) >= i_arg_B) begin
+            o_eqA = 1;
+
+            if (((i_arg_A >>> (M-1)) == 1) && ((i_arg_B >>> (M-1)) == 1) && ((o_eqA >>> (K-1)) == 0) ||
+            ((i_arg_A >>> (M-1)) == 0) && ((i_arg_B >>> (M-1)) == 0) && ((o_eqA >>> (K-1)) == 1))
+            begin
+                o_status[3] = 1;
+            end
+        end
+    end
+endmodule
+
+module division (i_arg_A, i_arg_B, o_div, o_status);
+    parameter M = 8;
+    parameter K = 8;
+    input logic signed [M-1:0] i_arg_A, i_arg_B;
+    output logic [K-1:0] o_div;
+    output logic [3:0] o_status;
+    logic signed [K-1:0] s_divid;
+    integer x;
+    integer a;
+
+    always_comb
+    begin
+        // Initialize outputs and variables
+        o_status = '0;
+        x = 0;
+        a = 0;
+        o_div = '0;
+
+         // If i_arg_B = 0, display an error
+        if (i_arg_B !== 0) 
+        begin
+            s_divid = i_arg_A / i_arg_B;
+            // Check if o_div is within the valid range
+            if (s_divid <= K-1) 
+            begin
+                o_div = s_divid;
+            end 
+            else
+                o_status[3] = 1; // Set status bit 3 to 1 if o_div is out of range
+        end 
+        else
+            o_status[0] = 1;
+
+        // Check if all bits in o_div are ones
+        if (o_div === (1 << K) - 1)
+            o_status[2] = 1; // Set status bit 2 to 1 if all bits in o_div are ones
+
+        // Count the number of '1' bits in o_div
+        for (a = 0; a < M; a++)
+        begin
+            if (o_div[a] == 1)
+                x = x + 1;
+        end
+
+        // Check if the number of '1' bits is even and o_div is not '0'
+        if (x % 2 == 0 && o_div != 0)
+            o_status[1] = 1; // Set status bit 1 to 1 if conditions are met
+        
+    end
+    
+endmodule
+
+module right_shift_2 (i_arg_A, i_arg_B, o_newA, o_status);
+    parameter M = 8;
+    parameter K = 8;
+    input logic signed [M-1:0] i_arg_A, i_arg_B;
+    output logic [K-1:0] o_newA;
+    output logic [3:0] o_status;
+    logic [M-1:0] s_s;
+    integer x;
+    integer a;
+
+    always_comb
+    begin
+        // Initialize outputs and variables
+        o_newA = '0;
+        x = 0;
+        a = 0;
+        o_status = '0;
+
+        // Check if i_argB is non-negative
+        if (i_arg_B >= 0)
+        begin
+            // Shift i_argA right by i_argB positions
+            s_s = ~i_arg_A >> i_arg_B;
+            
+            // Check if o_newA is within the valid range
+            if (s_s <= K-1)
+            begin
+                o_newA = s_s;
+            end
+            else
+                o_status[3] = 1; // Set status bit 3 to 1 if o_nawA is out of range
+        end
+        else
+            o_status[0] = 1; // Set status bit 0 to 1 if i_argB is negative
+
+        // Check if all bits in o_newA are ones
+        if (o_newA === (1 << K) - 1)
+            o_status[2] = 1; // Set status bit 2 to 1 if all bits in o_newA are ones
+
+        // Count the number of '1' bits in o_newA
+        for (a = 0; a < K; a++)
+        begin
+            if (o_newA[a] == 1)
+                x = x + 1;
+        end
+
+        // Check if the number of '1' bits is even and o_newA is not '0'
+        if (x % 2 == 0 && o_newA != 0)
+            o_status[1] = 1; // Set status bit 1 to 1 if conditions are met
+    end
+endmodule
+
+module add_bit(i_arg_A, i_arg_B, cache_status, cache_result);
+
+	parameter M = 8;
+	parameter K = 8;
+	
+	input logic [M-1:0] i_arg_A;
+	input logic [M-1:0] i_arg_B;
+	output logic [K-1:0] cache_result;
+	output logic [3:0] cache_status;
+	
+
+	logic [M-1:0] sum;
+	logic signed [M-1:0] s_A, s_B;
+	
+	always_comb begin
+		
+		cache_status = 4'b0;
+		cache_result = 8'b0;
+		
+		/* changing the inputs to signed so its easier to perform operations on them */
+        s_A = $signed(i_arg_A);
+        s_B = $signed(i_arg_B);
+        			 	 
+        			 	
+        /* checking for overflow and going out of bounds */
+        			 	
+        if ((s_A + s_B) > ((2**(M-1)) - 1)) begin
+        	cache_status = 4'b1001;
+    		cache_result = 8'bx;
+        end 
+        else if ((s_A + s_B) < -(2**(M-1))) begin
+        	cache_status = 4'b1001;
+    		cache_result = 8'bx;
+        end else begin
+        				
+        /* Adding up both input vectors, A and B */
+        				
+        sum = s_A + s_B;
+        				  
+        /*	Changing the corresponded to B value bit in sum to 0 
+        	keep in mind the bits are 0 indexed, so if B = 0001 then 
+        	in "sum" the bit changed will be the 2^1 one. */ 
+
+        sum = (sum & ~(1 << s_B));
+
+        /* Assigning the sum to the temporary result output */
+        				
+        cache_result = sum;
+        end
+	end
+endmodule
+
+module comparison_2(i_arg_A, i_arg_B, cache_status, cache_result);
+
+	parameter M = 8;
+	parameter K = 8;
+	
+	input logic [M-1:0] i_arg_A;
+	input logic [M-1:0] i_arg_B;
+	output logic [K-1:0] cache_result;
+	output logic [3:0] cache_status;
+	
+	logic [M-1:0] comp_A, comp_B;
+	
+	always_comb begin
+		
+		cache_status = 4'b0;
+		cache_result = 8'b0;
+		
+		/* if A is smaller than B then the output is NOT 0 */ 
+                    	
+        if ((i_arg_A[M-1] == 1) && (i_arg_B[M-1] == 0)) 
+        begin
+                		
+        	cache_result = 2'b01; 
+                    			
+        end else 
+                		
+        /* if A is bigger than B then the output is 0 */ 
+                		
+        if (((i_arg_A[M-1] == 0) && (i_arg_B[M-1] == 1)) || (i_arg_A == i_arg_B)) 
+        begin
+                		
+             cache_result = 0; // Output 0 if A >= B
+                    			
+        end else if ((i_arg_A[M-1] == 0) && (i_arg_B[M-1] == 0)) 
+        begin
+                		
+        	if (i_arg_A < i_arg_B) 
+        	begin
+                			
+        		cache_result = 2'b01; 
+                				
+        	end else 
+        	begin 
+                			
+            	cache_result = 0;
+                				
+        	end
+                			
+        end else if ((i_arg_A[M-1] == 1) && (i_arg_B[M-1] == 1)) 
+        begin
+            comp_A = i_arg_A;
+            comp_B = i_arg_B;
+                			
+            comp_A[M-1] = 0;
+            comp_B[M-1] = 0;
+                			
+        	if (comp_A > comp_B) 
+        	begin
+                			
+            	cache_result = 1;
+                			
+        	end else 
+        	begin
+                			
+            	cache_result = 0;
+                			
+        	end
+                		
+        end
+
+	end
+endmodule
+
+module sub_and_mul(i_arg_A, i_arg_B, cache_status, cache_result);
+
+	parameter M = 8;
+	parameter K = 8;
+	
+	input logic [M-1:0] i_arg_A;
+	input logic [M-1:0] i_arg_B;
+	output logic [K-1:0] cache_result;
+	output logic [3:0] cache_status;
+	
+
+	logic [M-1:0] cache;
+	logic [M-1:0] sum;
+	
+	logic signed [M-1:0] s_A, s_B;
+	
+	always_comb begin
+		
+		cache_status = 4'b0;
+		cache_result = 8'b0;
+		
+		cache = (i_arg_B << 1);
+    					
+    					
+    	/* changing the inputs to signed so its easier to perform operations on them */
+        s_A = $signed(i_arg_A);
+        s_B = $signed(cache);
+        				
+  		/* checking for overflow and going out of bounds */
+        if ((s_A - s_B) > ((2**(M-1)) - 1)) 
+        begin
+        				
+        	cache_status = 4'b1001;
+    		cache_result = 8'bx;
+    						
+        end 
+        else if ((s_A - s_B) < -(2**(M-1))) 
+        begin
+        				
+        	cache_status = 4'b1001;
+    		cache_result = 8'bx;
+    						
+        end else begin
+        				
+        /* if the overflow hasnt been detected, simply subtract the numbers and 
+        				   assign the value of subtraction to temporary result */
+        			
+        	sum = s_A - s_B;
+        	cache_result = sum;
+
+        end
+	end
+endmodule
+
+module u2_to_zm(i_arg_A, i_arg_B, cache_status, cache_result);
+
+	parameter M = 8;
+	parameter K = 8;
+	
+	input logic [M-1:0] i_arg_A;
+	input logic [M-1:0] i_arg_B;
+	output logic [K-1:0] cache_result;
+	output logic [3:0] cache_status;
+
+	
+	always_comb begin
+		
+		cache_status = 4'b0;
+		cache_result = 8'b0;
+		
+		/* if the number is negative, we invert all but first bits, then add 1 */
+    					
+    	if (i_arg_A == 8'b10000000) begin
+    					
+    		cache_status = 4'b1001;
+    		cache_result = {1'b1, ~(i_arg_A[M-2:0]) + 1'b1};
+    					
+    	end
+    	else if(i_arg_A[M-1] == 1) 
+    	begin
+    				
+        	cache_result = {1'b1, ~(i_arg_A[M-2:0]) + 1'b1};
+		
+    	end 
+    				
+    	/* if the number is positive, then the output is the number itself*/
+    				
+    	else begin
+    				
+        	cache_result = i_arg_A;
+        				
+    	end
+	end
+endmodule
+
+/*Poniżej widnieje zabytek starych czasów; na razie go nie usuwam*/
+
+//`include "./../SUBMODULES/MACIEK/addition.sv"
+//`include "./../SUBMODULES/MACIEK/division.sv"
+//`include "./../SUBMODULES/MACIEK/right_shift.sv"
+//`include "./../SUBMODULES/MACIEK/zm_to_u2.sv"
+
+//`include "./../SUBMODULES/ADAM/sub_and_mul.sv"
+//`include "./../SUBMODULES/ADAM/comparison_2.sv"
+//`include "./../SUBMODULES/ADAM/add_bit.sv"
+//`include "./../SUBMODULES/ADAM/u2_to_zm.sv"
+
+//`include "./../SUBMODULES/KACPER/absolute.sv"
+//`include "./../SUBMODULES/KACPER/comparison_1.sv"
+//`include "./../SUBMODULES/KACPER/division.sv"
+//`include "./../SUBMODULES/KACPER/right_shift_2.sv"
 
 module new_alu(i_op, i_arg_A, i_arg_B, i_clk, i_reset, o_result, o_status, o_op_rdy, o_alu_error);
 
--- proj/CODE/MODULES/testbench.sv
+++ proj/CODE/MODULES/testbench.sv
@@ -28,8 +28,11 @@ module testbench;
     initial begin
         $dumpfile("signals.vcd");
         $dumpvars(0, testbench);
+        
+        /*Operacje ALU Maćka*/
    
         /*1.) Testowanie operacji Y = (A >> ~B)*/
+        
         s_op = 4'b0000;
         s_reset = 1'b1;
 
@@ -37,6 +40,133 @@ module testbench;
         s_clk = 0;
         s_arg_A = 8'b11001100;
         s_arg_B = 8'b11111110;
+        #1
+        s_clk = 1;
+        #1
+        
+        /*2.) Testowanie operacji Y = A + ~B*/
+        
+        s_op = 4'b0001;
+        
+        /*Y = 2 + 3 (POPRAWNIE)*/
+        s_clk = 0;
+        s_arg_A = 8'b00000010;
+        s_arg_B = 8'b11111100;
+        #1
+        s_clk = 1;
+        #1
+		
+		/*3.) Testowanie operacji Y = A / B*/
+		
+		s_op = 4'b0010;
+        
+        /*Y = 13 / 3*/
+        s_clk = 0;
+        s_arg_A = 8'b00001101;
+        s_arg_B = 8'b00000011;
+        #1
+        s_clk = 1;
+        #1
+        
+        /*4.) Testowanie operacji ZM(A) => U2(A)*/
+        
+        s_op = 4'b0011;
+
+        /*Zamiana wartości -10 na U2*/
+        s_clk = 0;
+        s_arg_A = 8'b10001010;
+        #1
+        s_clk = 1;
+        #1
+        
+        
+        /*Operacje ALU Adama*/
+        
+        /*5.) Testowanie operacji Y = A-2*B*/
+        
+        s_op = 4'b0100;
+        
+        s_clk = 0;
+        s_arg_A = 8'b00000011;
+        s_arg_B = 8'b00000001;
+        #1
+        s_clk = 1;
+        #1
+        
+        /*6.) Testowanie operacji Y = A < B*/
+        
+        s_op = 4'b0101;
+        
+        s_clk = 0;
+        s_arg_A = 8'b00000110;
+        s_arg_B = 8'b00000011;
+        #1
+        s_clk = 1;
+        #1
+        
+        /*7.) Testowanie operacji Y = (A+B)[B]*/
+        
+        s_op = 4'b0110;
+        
+        s_clk = 0;
+        s_arg_A = 8'b00000011;
+        s_arg_B = 8'b00000010;
+        #1
+        s_clk = 1;
+        #1
+
+        /*8.) Testowanie operacji U2(A) => ZM(A)*/
+        
+        s_op = 4'b0111;
+        
+        s_clk = 0;
+        s_arg_A = 8'b11111001;
+        #1
+        s_clk = 1;
+        #1
+        
+        
+        /*Operacje ALU Kacpra*/
+        
+        /*9.) Testowanie operacji Y = ~A >> B*/
+        
+        s_op = 4'b1000;
+        
+        s_clk = 0;
+        s_arg_A = 8'b11111100;
+        s_arg_B = 8'b00001100;
+        #1
+        s_clk = 1;
+        #1
+        
+        /*10.) Testowanie operacji Y = ~A >= B*/
+        
+        s_op = 4'b1001;
+        
+        s_clk = 0;
+        s_arg_A = 8'b11111100;
+        s_arg_B = 8'b00000001;
+        #1
+        s_clk = 1;
+        #1
+        
+        /*11.) Testowanie operacji Y = ~A / B*/
+        
+        s_op = 4'b1010;
+        
+        s_clk = 0;
+        s_arg_A = 8'b11111000;
+        s_arg_B = 8'b00000001;
+        #1
+        s_clk = 1;
+        #1
+        
+        /*12.) Testowanie operacji Y = ~ABS(B)*/
+        
+        s_op = 4'b1010;
+        
+        s_clk = 0;
+        s_arg_B = 8'b01010101;
         #1
         s_clk = 1;
         #1
